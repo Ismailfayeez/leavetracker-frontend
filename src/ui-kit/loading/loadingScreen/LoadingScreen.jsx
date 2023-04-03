d8b6956fca5ea6526e6e92b8e5a9d022cb494541
mohammed ismail fayeez <mohammedismailfayeez@gmail.com>
2023-04-03 07:05:19 +0530

    added timer in loading screen

--- src/ui-kit/loading/loadingScreen/LoadingScreen.jsx
+++ src/ui-kit/loading/loadingScreen/LoadingScreen.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './loadingScreen.scss';
 
 function LoadingScreen() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
-    <div className="loading-screen flex flex--center">
+    <div className="loading-screen flex flex--center flex--column">
       <div className="loading-spinner-container">
         <span className="loader" />
       </div>
+      {seconds > 8 && <p style={{ textAlign: 'center' }}>Loading takes more than usual!</p>}
     </div>
   );
 }
