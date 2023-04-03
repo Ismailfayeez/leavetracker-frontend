de6f5252b07763f9fea4b6697088f89ae1a3d9c8
mohammed ismail fayeez <mohammedismailfayeez@gmail.com>
2023-04-03 17:20:40 +0530

    fixed loading issue in project owners route

--- src/apps/project/utilities/components/ProjectOwnerCheckRoute.jsx
+++ src/apps/project/utilities/components/ProjectOwnerCheckRoute.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import AccessDenied from '../../../../ui-kit/AccessDenied';
+import LoadingScreen from '../../../../ui-kit/loading/loadingScreen/LoadingScreen';
 import useProjectMemberPermission from '../hooks/useProjectMemberPermission';
 
-function ProjectOwnerCheckRoute({ component: Component, ...others }) {
+function ProjectOwnerCheckRoute({ component: Component, isLoading, ...others }) {
   const { isProjectMember, isProjectOwner } = useProjectMemberPermission();
+  if (isLoading) return <LoadingScreen />;
   // eslint-disable-next-line react/jsx-props-no-spreading
   if (isProjectMember && isProjectOwner) return <Component {...others} />;
   return <AccessDenied />;
